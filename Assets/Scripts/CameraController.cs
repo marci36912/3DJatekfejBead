d4efef10644b7278eaffe11e053924b3026168ce
Marci <lm4459533@gmail.com>
2024-02-29 18:02:46 +0100

    basic camera movement/rotation added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool w, a, s, d, scrollwheel;
+    private Vector3 rotation;
+    void Start()
+    {
+        rotation = new Vector3(0,0,0);
+    }
+
+    void Update()
+    {
+        getmovement();        
+    }
+
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    private void getmovement()
+    {
+        w = Input.GetKey(KeyCode.W);
+        a = Input.GetKey(KeyCode.A);
+        s = Input.GetKey(KeyCode.S);
+        d = Input.GetKey(KeyCode.D);
+        scrollwheel = Input.GetKey(KeyCode.Mouse2);
+    }
+
+    private void move()
+    {
+        if(w)
+        {
+            transform.position += (transform.forward * speed);
+        }
+        if(s)
+        {
+            transform.position += (-transform.forward * speed);
+        }
+        if(a)
+        {
+            transform.position += (-transform.right * speed);
+        }
+        if(d)
+        {
+            transform.position += (transform.right * speed);
+        }
+
+        if(scrollwheel)
+        {
+            rotation.y = Input.GetAxis("Mouse X");
+            transform.Rotate(rotation);
+        }
+    }
+}
