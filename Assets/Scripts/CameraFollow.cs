1ba2e2c6abda2a30a19863956fac3b439032fecf
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-04-16 22:13:18 +0200

    started to work on a camera mover for the car

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    private Transform car;
+    private Transform rotatePoint;
+    private bool backwards, left, right;
+
+    private void Start() 
+    {
+        car = GameObject.FindGameObjectWithTag("Car").transform;
+        rotatePoint = GameObject.Find("Camerarotator").transform;
+        transform.position = new Vector3(car.position.x + offset.x, car.position.y + offset.y, car.position.z + offset.z);
+    }
+
+    private void Update()
+    {
+        //ezekkel az auto is mozog, fixeld.
+        backwards = Input.GetKey(KeyCode.DownArrow);
+        left = Input.GetKey(KeyCode.LeftArrow);
+        right = Input.GetKey(KeyCode.RightArrow);
+
+        rotateCamera();
+    }
+
+    private void rotateCamera()
+    {
+        if(backwards)
+        {
+            rotatePoint.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else if(left)
+        {
+            rotatePoint.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else if (right)
+        {
+            rotatePoint.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else
+        {
+            rotatePoint.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
