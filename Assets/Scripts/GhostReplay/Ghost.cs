466ec1366545aaa8d75f254864998026b41c68ba
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-04-18 20:15:12 +0200

    working save system implemented for the ghost racer

--- Assets/Scripts/GhostReplay/Ghost.cs
+++ Assets/Scripts/GhostReplay/Ghost.cs
@@ -15,8 +15,23 @@ public class Ghost
         timestamp = time;
     }
 
-    public Ghost getData()
+    public Ghost(string line)
     {
-        return this;
+        string[] values = line.Split(' ');
+
+        if(values.Length == 7)
+            setValues(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]), float.Parse(values[3]), float.Parse(values[4]), float.Parse(values[5]), float.Parse(values[6]));
+    }
+
+    private void setValues(float positionX, float positionY, float positionZ, float rotationX, float rotationY, float rotationZ, float time)
+    {
+        position = new Vector3(positionX, positionY, positionZ);
+        rotation = new Vector3(rotationX, rotationY, rotationZ);
+        timestamp = time;
+    }
+
+    public override string ToString()
+    {
+        return $"{position.x} {position.y} {position.z} {rotation.x} {rotation.y} {rotation.z} {timestamp}";
     }
 }
--- Assets/Scripts/GhostReplay/GhostHolder.cs
+++ Assets/Scripts/GhostReplay/GhostHolder.cs
@@ -1,21 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class GhostHolder : MonoBehaviour
 {
     private static List<Ghost> _points;
+    private static string playerName;
+    private static string  playedDate;
+    private static readonly string fileName = "saves";
+
+    private void Start() 
+    {
+        loadSaves();
+        if(playerName == "" || playerName == null)
+            playerName = "John Doe";
+    }
 
     public void setPoints(List<Ghost> points)
     {
-        _points = points;
-
-        Debug.Log("Recording done");
+        if(_points != null)
+        { 
+            if((_points[_points.Count-1].timestamp - _points[0].timestamp) > (points[points.Count-1].timestamp - points[0].timestamp))
+            {
+                _points = points;
+                writeSaves();
+            }
+            
+            Debug.Log("Recording done");
+        }
+        else
+        {
+            _points = points;
+            writeSaves();   
+        }
     }
 
     public List<Ghost> getPoints()
     {
         Debug.Log("points set");
         return _points;
+    }
+
+    public static void setName(string name)
+    {
+        playerName = name;
+    }
+
+    public static void setDate()
+    {
+        playedDate = DateTime.Now.ToString("yyyy/MM/dd");
+    }
+
+    public void writeSaves()
+    {
+        if(playedDate == "" || playedDate == null)
+            setDate();
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            outputFile.WriteLine($"{playerName} {playedDate}\n");
+            foreach (Ghost point in _points)
+            {
+                outputFile.WriteLine(point.ToString());
+            }
+
+            outputFile.Close();
+        }
+
+        Debug.Log("Data written");
+    }
+
+    public void loadSaves()
+    {
+        if(File.Exists(fileName))
+        {
+            _points = new List<Ghost>();
+            using(StreamReader file = new StreamReader(fileName)) 
+            {
+                string line = file.ReadLine();
+                string[] firstLine = line.Split(' ');
+
+                playerName = firstLine[0];
+                playedDate = firstLine[1];
+
+                while((line = file.ReadLine()) != null) 
+                {
+                    _points.Add(new Ghost(line));
+                }
+
+                file.Close();
+            }
+
+            Debug.Log("Data loaded");
+        }
     }
 }
